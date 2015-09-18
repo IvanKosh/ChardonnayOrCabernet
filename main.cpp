44eb1a7d982b0dd51a6bef1f11977507875c1739
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-18 10:40:48 +0300

    fail

--- main.cpp
+++ main.cpp
@@ -7,80 +7,82 @@
 
 #include <iostream>
 #include <fstream>
-#include <string>
-//#include <sys/types.h>
-//#include <regex.h>
-
-
 #include <regex.h>
-
-//#include <boost/regex.hpp>
 
 using namespace std;
 
-int match(const char *string, char *pattern)
-{
-    int    status;
-    regex_t    re;
-
-    if (regcomp(&re, pattern, REG_EXTENDED|REG_NOSUB) != 0) {
-		cout << "error1";
-        return(0);      /* report error */
-    }
-    status = regexec(&re, string, (size_t) 0, NULL, 0);
-    regfree(&re);
-    if (status != 0) {
-		cout << "error2";
-        return(0);      /* report error */
-    }
-    return(1);
-}
-
-
 int main(int argc, char *argv[]) {
 	ifstream stream(argv[1]);
-	string line;
+	string line, temp;
+	
+	regex_t regex;
+	int reti;
+	char msgbuf[16];
+	short i, j, l;
+	bool test;
+	
 	while (getline(stream, line)) {
-		cout << line << endl;
+		// найти позицию " | "
+		temp = "^[a-z]*";
+		test = true;
+		l = line.length();
+		for (i = l; line[--i] != '|';) {
+		}
+		
+		// составить регулярное выражение на основе правой части
+		for (j = i+2; j < l; j++) {
+			//cout << line[j] << '*';
+			temp += line[j];
+			temp += "[a-z]*";
+		}
+		temp += '$';
+		
+		//cout << temp << endl;
+		
+		reti = regcomp(&regex, temp.c_str(), REG_ICASE);
+		if (reti) {
+			fprintf(stderr, "Could not compile regex\n");
+			return 1;
+		}
+		
+		// для каждого слова левой части заюзать регулярное выражение
+		l = i;
+		j = 0;
+		for (i = 0; i < l; i++) {
+			if (line[i] == ' ') {
+//				cout << '[' << line.substr(j, i-j) << ']';
+//				
+//				//char_temp = line.substr(j, i-j).c_str();
+//				cout << line.substr(j, i-j).c_str() << endl;
+//				
+//				//reti = regexec(&regex, "Merlot", 0, NULL, 0);
+				reti = regexec(&regex, line.substr(j, i-j).c_str(), 0, NULL, 0);
+//				cout << reti;
+				if (!reti) {
+					cout << line.substr(j, i-j+1);
+					test = false;
+				}
+				else if (reti == REG_NOMATCH) {
+					//cout << "No match";
+				}
+				else {
+					regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+					fprintf(stderr, "Regex match failed: %s\n", msgbuf);
+					cout << "Error";
+					return 1;
+				}
+				
+				
+				j = i+1;
+			}
+		}
+		
+		// если test == true вывести False
+		if (test) {
+			cout << "False";
+		}
+		cout << endl;
+		//cout << endl << line << endl;
 	}
-	
-	//regex integer;
-	
-	string input;
-	//regex_t integer;
-	//integer = new regex_t("(\\+|-)?[[:digit:]]+");
-	
-	regex_t reg;
-	regcomp(&reg, "a|b", REG_EXTENDED|REG_NOSUB);
-	
-	cout << regexec(&reg, "a", (size_t) 0, NULL, 0);
-	cout << regexec(&reg, "b", (size_t) 0, NULL, 0);
-	cout << regexec(&reg, "ab", (size_t) 0, NULL, 0);
-	
-	regfree(&reg);
-	cout << match("a", "a|b") << endl;
-	cout << match("c", "a|b") << endl;
-	
-	//reg
-	
-	//int regcomp(regex_t *preg, const char *regex, int cflags);
-	//cout << regcomp(integer, "123", 1);
-	
- 	//As long as the input is correct ask for another number
-// 	while(true)
-// 	{
-// 		cout<<"Give me an integer!"<<endl;
-// 		cin>>input;
-// 		if(!cin) break;
-// 		//Exit when the user inputs q
-// 		if(input=="q")
-// 			break;
-// 		if(reg regex_match(input, integer))
-// 			cout<<"integer"<<endl;
-// 		else
-// 		{
-// 			cout<<"Invalid input"<<endl;
-// 		}
-// 	}
 	return 0;
 }
